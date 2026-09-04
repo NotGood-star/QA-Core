1110f5dd283b79e43f5439884872e569b4ca8a37
Not_Good <reetujoshi588@gmail.com>
2026-09-04 20:24:17 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,59 @@
+export const CONFIG = {
+  roles: {
+    developer: "1544342276548071425",
+    tester: "1544342096763158538"
+  },
+
+  channels: {
+    paid: "1544362274523979926",
+    upcoming: "1545035124272267386",
+    host: "1545035189363806348",
+    volunteer: "1545040569297010728"
+  },
+
+  emojis: {
+    qa_logo: "<:QA:1545389852324008037>",
+    qa_test: "<a:GeneralSupport:1545361251620618271>",
+    qa_tester: "<:Tester:1545384454732779601>",
+    qa_developer: "<:Developer:1545384357869396008>",
+    qa_verified: "<a:Verified:1545072847045525614>",
+
+    qa_bug: "<:Bug:1545071810242940951>",
+    qa_feedback: "<a:Feedback:1545387031851438202>",
+    qa_report: "<:Report:1545387063338344549>",
+
+    qa_check: "<a:Tick:1545072762396344451>",
+    qa_cross: "<a:Animated_Cross:1545073838725791805>",
+    qa_pending: "<a:pending:1545074332273741864>",
+    qa_warning: "<:Warning:1545074413702090875>",
+
+    qa_robux: "<:Robux:1544663266687586354>",
+    qa_paid: "<:Paid_User:1545074498959839323>",
+    qa_free: "<:FreeWorldAngel:1545074663573688380>",
+    qa_reward: "<a:rewards:1545074806364315649>",
+
+    qa_ticket: "<a:Tickets:1545074985175883866>",
+
+    qa_roblox: "<:Roblox:1545295365685911663>",
+    qa_game: "<a:GAMES:1545295520216522834>",
+
+    qa_pc: "💻",
+    qa_mobile: "📱",
+    qa_console: "🎮",
+
+    qa_calendar: "📅",
+    qa_clock: "🕐",
+
+    qa_live: "<a:online:1545296013026267288>",
+    qa_upcoming: "<a:Fire:1545391015739924560>",
+
+    qa_upcoslots: "<:Star:1545296442405429298>",
+    qa_slots: "<:Member:1545296603341000706>",
+
+    qa_apply: "📝",
+    qa_join: "➕",
+    qa_close: "🔒"
+  }
+};
+
+export const COLOR = 0x6C4DFF;
