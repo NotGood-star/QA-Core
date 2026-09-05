8ae3bdbd0ab2a78e4ada04c36dc2e8599c452e42
Not_Good <reetujoshi588@gmail.com>
2026-09-05 19:50:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import http from "node:http";
 import { Client, GatewayIntentBits } from "discord.js";
 import { registerEvents } from "./events/index.js";
+
+const PORT = process.env.PORT || 3000;
 
 const client = new Client({
   intents: [
@@ -8,5 +11,18 @@ const client = new Client({
 });
 
 registerEvents(client);
+
+// Render health server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("QA Central is online!");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🌐 Health server running on port ${PORT}`);
+});
 
 client.login(process.env.DISCORD_TOKEN);
