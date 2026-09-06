f8ece07afbad4bfa9d58488dd578b9f4e4a340a8
Not_Good <reetujoshi588@gmail.com>
2026-09-06 18:29:34 +0530

    Update index.js

--- src/events/index.js
+++ src/events/index.js
@@ -2,7 +2,7 @@ import { ready } from "./ready.js";
 import { interaction } from "./interaction.js";
 
 export function registerEvents(client) {
-  client.once("ready", () => ready(client));
+  client.once("clientReady", () => ready(client));
 
   client.on("interactionCreate", (interactionObject) => {
     interaction(client, interactionObject);
